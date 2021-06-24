6c3f9d13b5a8bacacc5fe15de04143953017aa57
Fanis <fanis.kolokythas@gmail.com>
2021-06-25 00:24:12 +0300

    Prop

--- testing/tut/src/index.js
+++ testing/tut/src/index.js
@@ -2,45 +2,39 @@ import React from 'react';
 import ReactDom from "react-dom"
 import './index.css';
 
+//vars
+const firstBook = {
+   img: "https://images-na.ssl-images-amazon.com/images/I/713mzPe9SwS._AC_UL200_SR200,200_.jpg",
+   title: "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones",
+   author: "James Clear (Author)"
+}
+
+const secondBook = {
+   img: "https://images-na.ssl-images-amazon.com/images/I/81BdMSuI5ZS._AC_UL200_SR200,200_.jpg",
+   title: "The Last Thing He Told Me",
+   author: "Laura Dave (Author)"
+}
+
+
 function BooκList() {
    return (
       <section className="booklist">
-         <Book />
-         <Book />
-         <Book />
-         <Book />
-         <Book />
-         <Book />
-
+         <Book img={firstBook.img} title={firstBook.title} author={firstBook.author} />
+         <Book img={secondBook.img} title={secondBook.title} author={secondBook.author} />
       </section>
 
    );
 }
 
-const Book = () => {
+const Book = ({ props }) => {
+   const { img, title, author } = props
    return (
       <article className="book">
-         <Image />
-         <Title />
-         <Author />
+         <img src={img} alt="" />
+         <h1>{title}</h1>
+         <h4>{author}</h4>
       </article>
    );
 };
-
-const Image = () => (
-   <img
-      src="https://images-na.ssl-images-amazon.com/images/I/713mzPe9SwS._AC_UL200_SR200,200_.jpg" alt=""
-   />
-)
-
-const Title = () => (
-   <h3>Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones</h3>
-)
-
-const Author = () => (
-   <h1> James Clear (Author) </h1>
-)
-
-
 
 ReactDom.render(<BooκList />, document.getElementById('root'));
