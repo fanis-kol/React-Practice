cf4fc9ba865cbb684af740eb405d86d77f2a6cc6
Fanis <fanis.kolokythas@gmail.com>
2021-07-19 16:31:07 +0300

    test

--- testing/tut/src/index.js
+++ testing/tut/src/index.js
@@ -5,16 +5,19 @@ import './index.css';
 //vars
 const books = [
    {
+      id: 1,
       img: "https://images-na.ssl-images-amazon.com/images/I/713mzPe9SwS._AC_UL200_SR200,200_.jpg",
       title: "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones",
       author: "James Clear (Author)"
    },
    {
+      id: 2,
       img: "https://images-na.ssl-images-amazon.com/images/I/81BdMSuI5ZS._AC_UL200_SR200,200_.jpg",
       title: "The Last Thing He Told Me",
       author: "Laura Dave (Author)"
    },
    {
+      id: 3,
       img: "https://images-na.ssl-images-amazon.com/images/I/61NdJMwAThS._AC_UL200_SR200,200_.jpg://images-na.ssl-images-amazon.com/images/I/81BdMSuI5ZS._AC_UL200_SR200,200_.jpg",
       title: "The Body Keeps the Score: Brain, Mind, and Body in the Healing of Trauma",
       author: " Bessel van der Kolk M.D (Author)"
@@ -26,9 +29,8 @@ function BooκList() {
    return (
       <section className="booklist">
          {books.map((book) => {
-            const { img, title, author } = book;
             return (
-               <Book book={book}></Book>
+               <Book key={book.id} {...book}></Book>
             );
          })}
       </section>
@@ -36,8 +38,8 @@ function BooκList() {
    );
 }
 
-const Book = (props) => {
-   const { img, title, author } = props.book;
+const Book = ({ img, title, author }) => {
+   //const { img, title, author } = props;
    return (
       <article className="book">
          <img src={img} alt="" />
