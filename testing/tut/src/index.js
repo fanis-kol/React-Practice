c03e7e335ca8e6216f17150251d926471a3c4556
Fanis <fanis.kolokythas@gmail.com>
2021-07-26 23:14:12 +0300

    event

--- testing/tut/src/index.js
+++ testing/tut/src/index.js
@@ -39,12 +39,17 @@ function BooκList() {
 }
 
 const Book = ({ img, title, author }) => {
-   //const { img, title, author } = props;
+   const clickHandler = () => {
+      alert("hello world");
+   }
    return (
       <article className="book">
          <img src={img} alt="" />
          <h1>{title}</h1>
          <h4>{author}</h4>
+         <button type="button" onClick={clickHandler}>
+            reference example
+         </button>
       </article>
    );
 };
